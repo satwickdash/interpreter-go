6f1682dce40e6180382f4fd595b7bbe250478e1d
Satwick Dash <satwick1995@gmail.com>
2024-08-28 21:03:42 +0530

    Init first commit, lexer tests working.

---
+++ lexer/lexer.go
@@ -0,0 +1,62 @@
+package lexer
+
+import "interpreter-go/token"
+
+type Lexer struct {
+	input        string
+	position     int  // Current position in input
+	readPosition int  // Current reading position (after position)
+	ch           byte // Current character being considered (at position)
+}
+
+func (l *Lexer) readChar() {
+	if l.readPosition >= len(l.input) {
+		l.ch = 0
+	} else {
+		l.ch = l.input[l.readPosition]
+	}
+	l.position = l.readPosition
+	l.readPosition += 1
+}
+
+func New(inp string) *Lexer {
+	l := &Lexer{input: inp}
+	l.readChar()
+	return l
+}
+
+func (l *Lexer) NextToken() token.Token {
+	var tok token.Token
+
+	switch l.ch {
+	case '=':
+		tok = newToken(token.ASSIGN, l.ch)
+	case ';':
+		tok = newToken(token.SEMICOLON, l.ch)
+	case '(':
+		tok = newToken(token.LPAREN, l.ch)
+	case ')':
+		tok = newToken(token.RPAREN, l.ch)
+	case ',':
+		tok = newToken(token.COMMA, l.ch)
+	case '+':
+		tok = newToken(token.PLUS, l.ch)
+	case '{':
+		tok = newToken(token.LBRACE, l.ch)
+	case '}':
+		tok = newToken(token.RBRACE, l.ch)
+	case 0:
+		tok.Literal = ""
+		tok.Type = token.EOF
+	}
+
+	l.readChar()
+	return tok
+}
+
+func newToken(tt token.TokenType, ch byte) token.Token {
+	return token.Token{
+		Type:    tt,
+		Literal: string(ch),
+	}
+}
